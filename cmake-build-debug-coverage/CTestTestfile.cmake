049033838042a4e3a59319d846b5d09dc446f3b4
Андрей Лисин <foxers100@gmail.com>
2021-03-15 00:00:53 +0300

    deleted cmake-build-debug-coverage

--- cmake-build-debug-coverage/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/foxers/CLionProjects/advcpp2021homework1
-# Build directory: /home/foxers/CLionProjects/advcpp2021homework1/cmake-build-debug-coverage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtests "./tests")
-set_tests_properties(gtests PROPERTIES  _BACKTRACE_TRIPLES "/home/foxers/CLionProjects/advcpp2021homework1/CMakeLists.txt;18;add_test;/home/foxers/CLionProjects/advcpp2021homework1/CMakeLists.txt;0;")
